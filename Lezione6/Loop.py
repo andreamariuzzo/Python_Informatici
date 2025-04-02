cb7562610f204cd9757a75cff639dda16510e296
andreamariuzzo <146810487+andreamariuzzo@users.noreply.github.com>
2025-04-02 09:59:56 +0200

    Add files via upload

---
+++ Lezione6/Loop.py
@@ -0,0 +1,14 @@
+# Con i Loop possiamo ripetere dei blocchi di codice più volte
+
+# LOOP INDEFINITI: sono dei loop che potrebbere ciclare all'infinito
+
+# while condizioneTrue:
+#     fai qualcosa
+
+#while True:
+#    print("Ciao")  # Stampa Ciao all'infinito
+
+numStop = 0
+while numStop < 10:
+    numStop += 1  
+    print("Adesso il numero vale: ", numStop)
